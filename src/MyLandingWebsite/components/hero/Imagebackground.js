6eb3a119e49b5e084094b52a5a6b8a6f0857ae7f
shanmukh <shanmukh.tata@smartfalcon.io>
2024-02-23 15:32:16 +0530

    changed h1 to h2 to remove h1 tag conflict

--- src/MyLandingWebsite/components/hero/Imagebackground.js
+++ src/MyLandingWebsite/components/hero/Imagebackground.js
@@ -15,7 +15,7 @@ const OpacityOverlay = tw.div`z-10 absolute inset-0 bg-black opacity-75`;
 const HeroContainer = tw.div`z-20 relative px-6 sm:px-8 mx-auto h-full flex flex-col`;
 const Content = tw.div`px-4 flex flex-1 flex-col justify-center items-center`;
 
-const Heading = styled.h1`
+const Heading = styled.h2`
   ${tw`text-3xl text-center sm:text-4xl lg:text-5xl xl:text-6xl font-black text-gray-100 leading-snug -mt-24 sm:mt-0`}
   span {
     ${tw`inline-block mt-2`}
